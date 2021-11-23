905f588b3c375f4ac53e89cac15faec78f45d6e7
sridi17 <94300992+sridi17@users.noreply.github.com>
2021-11-23 08:08:44 +0530

    Update test.cs

--- 3.Implementation/test/test.cs
+++ 3.Implementation/test/test.cs
@@ -1,1 +1,24 @@
-
+// Function to draw the boundaries
+void draw()
+{
+    system("cls");
+    for (i = 0; i < height; i++) {
+        for (j = 0; j < width; j++) {
+            if (i == 0 || i == width - 1
+                || j == 0
+                || j == height - 1) {
+                printf("#");
+            }
+            else {
+                if (i == x && j == y)
+                    printf("0");
+                else if (i == fruitx
+                         && j == fruity)
+                    printf("*");
+                else
+                    printf(" ");
+            }
+        }
+        printf("\n");
+    }
+  
