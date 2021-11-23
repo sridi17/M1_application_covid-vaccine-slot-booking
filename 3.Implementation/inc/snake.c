a381fe8db6db96dd5b888dd959e77a9d342bf8b1
sridi17 <94300992+sridi17@users.noreply.github.com>
2021-11-23 08:01:52 +0530

    Update snake.c

--- 3.Implementation/inc/snake.c
+++ 3.Implementation/inc/snake.c
@@ -1,1 +1,25 @@
+// C program to build the outline
+// boundary using draw()
+#include <stdio.h>
+#include <stdlib.h>
+int i, j, height = 30;
+int width = 30, gameover, score;
+  
+// Function to draw a boundary
+void draw()
+{
+    // system("cls");
+    for (i = 0; i < height; i++) {
+        for (j = 0; j < width; j++) {
+            if (i == 0 || i == width - 1 || j == 0
+                || j == height - 1) {
+                printf("#");
+            }
+            else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
 
